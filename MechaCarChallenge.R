12d62a66cb5f0a17c38fdef780ffb6ae59786f1d
greensleeves8 <theonlycalegreen@gmail.com>
2021-01-17 21:58:48 -0600

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,14 @@ suspension_coil <- read.csv(file='Suspension_coil.csv',check.names = F,stringsAs
 total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+sample_table <- suspension_coil %>% sample_n(50)
+
+t.test(sample_table$PSI,mu=mean(suspension_coil$PSI))
+
+t.test(subset(suspension_coil$PSI,suspension_coil$Manufacturing_Lot == "Lot1"),mu=mean(suspension_coil$PSI))
+
+t.test(subset(suspension_coil$PSI,suspension_coil$Manufacturing_Lot == "Lot2"),mu=mean(suspension_coil$PSI))
+
+t.test(subset(suspension_coil$PSI,suspension_coil$Manufacturing_Lot == "Lot3"),mu=mean(suspension_coil$PSI))
+
